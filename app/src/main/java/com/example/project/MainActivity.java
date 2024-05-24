748bc12dd14c169893a3844cfd859405cfc8ee25
b22robhe <96245488+b22robhe@users.noreply.github.com>
2024-05-24 19:51:56 +0200

    GSON and adapter imported the GSON library to parse the json data and store it in the array create the adapter and set it to the view

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.project;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
@@ -16,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
 
     RecyclerViewAdapter adapter;
 
+    RecyclerView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,10 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        tests = new ArrayList<>();
+
+
         new JsonTask(this).execute(JSON_URL);
     }
 
@@ -31,7 +44,26 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     public void onPostExecute(String json) {
 
         Log.d("MainActivity", json);
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Test>>() {}.getType();
+        tests = gson.fromJson(json, type);
 
+        //Log.d("MainActivity", tests.toString());
+
+        adapter = new RecyclerViewAdapter(this, tests, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Test item) {
+                Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        view = findViewById(R.id.recycler_view);
+        view.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        view.setAdapter(adapter);
+
+
+
+        this.adapter.notifyDataSetChanged();
 
 
     }
