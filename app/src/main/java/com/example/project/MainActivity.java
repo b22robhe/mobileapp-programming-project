b26c3ad926eb32cb30880756dd787c7033f4b2c1
b22robhe <96245488+b22robhe@users.noreply.github.com>
2024-05-24 19:05:30 +0200

    Main Activity functionality made the class implement JsonTask added Override for "onPostExecute" added variable for adapter and test array

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b22robhe";
+
+    ArrayList<Test> tests;
+
+    RecyclerViewAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+
+        Log.d("MainActivity", json);
+
+
+
     }
 
 }
